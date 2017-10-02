59c31f2c70c767676bfc038109ddb68c03f99498
Cheney Shreve <cheney.shreve@gmail.com>
2017-10-02 22:19:16 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,9 +11,6 @@ end
 # code your input_to_index and move method here!
 def input_to_index(user_input)
   index = user_input.to_i - 1
-  if user_input = ""
-    index = -1
-  end
   return index
 end
 
